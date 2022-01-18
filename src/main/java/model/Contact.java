2f863af19d5169012a1faf6ae47b0dd6573cd8f0
rksgr <r_kjha@yahoo.com>
2022-01-18 18:40:27 +0530

    Contact class containing contact details attributes and their getters and setters

---
+++ src/main/java/model/Contact.java
@@ -0,0 +1,88 @@
+package model;
+
+public class Contact {
+    private String first_name;
+    private String last_name;
+    private String address;
+    private String city;
+    private String state;
+    private int zip;
+    private double phone_num;
+    private String email;
+
+    public Contact(String first_name, String last_name, String address, String city, String state, int zip,
+                   double phone_num, String email) {
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.phone_num = phone_num;
+        this.email = email;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public int getZip() {
+        return zip;
+    }
+
+    public void setZip(int zip) {
+        this.zip = zip;
+    }
+
+    public double getPhone_num() {
+        return phone_num;
+    }
+
+    public void setPhone_num(double phone_num) {
+        this.phone_num = phone_num;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
---
+++ src/main/java/service/AddressBookService.java
@@ -0,0 +1,16 @@
+package service;
+
+import model.Contact;
+
+import java.util.ArrayList;
+
+public class AddressBook {
+    ArrayList
+
+    /*
+    Use Case 1: Create a Contact in address book containing basic details
+     */
+    public void createContact(Contact contact){
+
+    }
+}
