c174648eeeb8917390516dc6c126ca929e1f0fad
rksgr <r_kjha@yahoo.com>
2022-01-18 19:15:12 +0530

    Contains main method which call add contact using console to check if contact added

--- src/main/java/AddressBookMain.java
+++ src/main/java/AddressBookMain.java
@@ -1,5 +1,16 @@
+import model.AddressBook;
+import service.AddressBookService;
+
 public class AddressBookMain {
     public static void main(String[] args) {
         System.out.println("Today we shall make Address Book System!");
+        AddressBookService addressBookService = new AddressBookService();
+        AddressBook addressBook = new AddressBook();
+        int size1 = addressBook.getAddressBookList().size();
+
+        addressBookService.addContactUsingConsole(addressBook);
+        int size2 = addressBook.getAddressBookList().size();
+        System.out.println(" size before addition = "+size1);
+        System.out.println(" size after addition = "+size2);
     }
 }
