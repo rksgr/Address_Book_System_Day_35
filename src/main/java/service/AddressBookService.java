a93eac52ed2f25d4de078e475bd3adaa93ee04d4
rksgr <r_kjha@yahoo.com>
2022-01-18 18:42:44 +0530

    Added method to create a contact in address book

--- src/main/java/service/AddressBookService.java
+++ src/main/java/service/AddressBookService.java
@@ -1,16 +1,16 @@
 package service;
 
+import model.AddressBook;
 import model.Contact;
 
-import java.util.ArrayList;
-
-public class AddressBook {
-    ArrayList
+public class AddressBookService {
 
     /*
-    Use Case 1: Create a Contact in address book containing basic details
+    Use Case 1: Create a Contact in address book containing contact details
      */
-    public void createContact(Contact contact){
-
+    public AddressBook createContact(AddressBook addressBook, Contact contact){
+        // Adds the contact to the ArrayList
+        addressBook.getAddressBookList().add(contact);
+        return addressBook;
     }
 }
