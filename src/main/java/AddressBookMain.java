db24209b4b2136739473cf823460031190ce2383
rksgr <r_kjha@yahoo.com>
2022-01-18 23:46:16 +0530

    Main method to check whether multiple persons added to the address book

--- src/main/java/AddressBookMain.java
+++ src/main/java/AddressBookMain.java
@@ -5,11 +5,14 @@ public class AddressBookMain {
     public static void main(String[] args) {
         System.out.println("Today we shall make Address Book System!");
         AddressBookService addressBookService = new AddressBookService();
+
         AddressBook addressBook = new AddressBook();
+
+        // Get the size before and after addition of contact
         int size1 = addressBook.getAddressBookList().size();
 
-        addressBookService.addContactUsingConsole(addressBook);
-        int size2 = addressBook.getAddressBookList().size();
+
+        int size2 = addressBookService.addMultiplePerson(addressBook,3).getAddressBookList().size();
         System.out.println(" size before addition = "+size1);
         System.out.println(" size after addition = "+size2);
     }
