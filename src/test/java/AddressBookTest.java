3af240833820571b1d47935996b97bef8455525c
rksgr <r_kjha@yahoo.com>
2022-01-18 20:52:32 +0530

    Added new test method to check if existing contact edited using person name

--- src/test/java/AddressBookTest.java
+++ src/test/java/AddressBookTest.java
@@ -23,4 +23,27 @@ public class AddressBookTest {
         int size2 = addressBookService.createContact(addressBook,contact).getAddressBookList().size();
         Assertions.assertEquals(size2,size1+1);
     }
+    /*
+    Use Case 3: Edit existing contact in address book using name of person
+     */
+    @Test
+    public void whenContactEdited_addressBookEntry_IsEdited(){
+        Contact contact = new Contact("Arun","kumar","Leadenhall Street","London",
+                "England",111,89898989,"arun.kumar@gmail.com");
+        Contact contact_edited = new Contact("Arun","kumar","Leadenhall Street","London",
+                "England",5555,77777777,"arun.kumar1234@gmail.com");
+        AddressBook addressBook = new AddressBook();
+        AddressBookService addressBookService = new AddressBookService();
+
+        // Add a contact to the address book
+        addressBookService.createContact(addressBook,contact);
+
+        // Edit contact
+        addressBookService.editExistingContact(addressBook,"Arun","kumar",contact_edited);
+
+        // fetch zip of the edited contact
+        int zip_edited = addressBook.getAddressBookList().get(0).getZip();
+
+        Assertions.assertEquals(zip_edited,5555);
+    }
 }
