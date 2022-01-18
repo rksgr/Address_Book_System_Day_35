920fa427261b2fee2d30274de8f5af49b4ea0d1e
rksgr <r_kjha@yahoo.com>
2022-01-18 18:43:48 +0530

    Added method to test creation of a contact in address book

--- src/test/java/AddressBookTest.java
+++ src/test/java/AddressBookTest.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class AddressBookTest {
+import model.AddressBook;
+import model.Contact;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import service.AddressBookService;
+
+public class AddressBookTest {
+
+    /*
+    Use Case 1: Create a contacts in address book
+     */
+    @Test
+    public void whenContactCreated_sizeOfAddressBook_IncreasesByOne(){
+        Contact contact = new Contact("Arun","kumar","Leadenhall Street","London",
+                "England",111,89898989,"arun.kumar@gmail.com");
+        AddressBook addressBook = new AddressBook();
+        AddressBookService addressBookService = new AddressBookService();
+
+        // size of list before contact creation
+        int size1 =  addressBook.getAddressBookList().size();
+
+        // size of list after contact creation
+        int size2 = addressBookService.createContact(addressBook,contact).getAddressBookList().size();
+        Assertions.assertEquals(size2,size1+1);
+    }
 }
