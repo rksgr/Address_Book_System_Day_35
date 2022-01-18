36b7387fe4ca96c9dcf24d2144b55f901aa7895a
rksgr <r_kjha@yahoo.com>
2022-01-18 18:41:36 +0530

    Added AddressBook Class - contains AddressBook list

--- src/main/java/model/AddressBook.java
+++ src/main/java/model/AddressBook.java
@@ -1,2 +1,15 @@
-package model;public class AddressBook {
+package model;
+
+import java.util.ArrayList;
+
+public class AddressBook {
+    private ArrayList<Contact> addressBookList = new ArrayList<>();
+
+    public ArrayList<Contact> getAddressBookList() {
+        return addressBookList;
+    }
+
+    public void setAddressBookList(ArrayList<Contact> addressBookList) {
+        this.addressBookList = addressBookList;
+    }
 }
