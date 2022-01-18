5694d558a6edcf2da40bfdb46f6271fd876d1f7a
rksgr <r_kjha@yahoo.com>
2022-01-19 00:43:54 +0530

    Main method to check whether new entries added to the addressbook system

--- src/main/java/AddressBookMain.java
+++ src/main/java/AddressBookMain.java
@@ -1,18 +1,22 @@
 import model.AddressBook;
+import model.AddressBookSystem;
 import service.AddressBookService;
 
 public class AddressBookMain {
     public static void main(String[] args) {
         System.out.println("Today we shall make Address Book System!");
+        AddressBookSystem addressBookSystem = new AddressBookSystem();
         AddressBookService addressBookService = new AddressBookService();
 
         AddressBook addressBook = new AddressBook();
 
-        // Get the size before and after addition of contact
-        int size1 = addressBook.getAddressBookList().size();
+        // Get the size of addressBookSystem before and after addition of address books
+        int size1 = addressBookSystem.getAddressBookMap().size();
 
-
-        int size2 = addressBookService.addMultiplePerson(addressBook,3).getAddressBookList().size();
+        addressBookService.addMultipleAddressBook(addressBookSystem,1);
+        //int size2 = addressBookService.addMultipleAddressBook(addressBookSystem,3)
+          //          .getAddressBookMap().size();
+        int size2 = addressBookSystem.getAddressBookMap().size();
         System.out.println(" size before addition = "+size1);
         System.out.println(" size after addition = "+size2);
     }
