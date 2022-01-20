6d850aea4b10fcf900401fe01410c255f6f3f232
rksgr <r_kjha@yahoo.com>
2022-01-20 06:28:28 +0530

    Refactored: Added a default and single parameter constructor

--- src/main/java/model/AddressBook.java
+++ src/main/java/model/AddressBook.java
@@ -4,6 +4,21 @@ import java.util.ArrayList;
 
 public class AddressBook {
     private ArrayList<Contact> addressBookList = new ArrayList<>();
+    private String addr_book_name;
+    public AddressBook(){}
+
+    // Single parameter constructor
+    public AddressBook(String addr_book_name){
+        this.addr_book_name = addr_book_name;
+    }
+    
+    public String getAddr_book_name() {
+        return addr_book_name;
+    }
+
+    public void setAddr_book_name(String addr_book_name) {
+        this.addr_book_name = addr_book_name;
+    }
 
     public ArrayList<Contact> getAddressBookList() {
         return addressBookList;
