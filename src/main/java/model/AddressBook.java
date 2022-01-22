c15f938f418fefff200fa349982119eebf33d817
rksgr <r_kjha@yahoo.com>
2022-01-22 07:35:35 +0530

    No change

--- src/main/java/model/AddressBook.java
+++ src/main/java/model/AddressBook.java
@@ -5,12 +5,14 @@ import java.util.ArrayList;
 public class AddressBook {
     private ArrayList<Contact> addressBookList = new ArrayList<>();
     private String addr_book_name;
+
     public AddressBook(){}
 
     // Single parameter constructor
     public AddressBook(String addr_book_name){
         this.addr_book_name = addr_book_name;
     }
+
 
     public String getAddr_book_name() {
         return addr_book_name;
