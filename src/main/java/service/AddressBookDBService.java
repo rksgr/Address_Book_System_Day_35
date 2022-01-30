d00bc34058ebb6aaf2a5a09cf8b58ecebbc6fe43
rksgr <r_kjha@yahoo.com>
2022-01-30 13:22:50 +0530

    Refactored: Method to update entry in database and another method to verify if the entry in database same as memory

--- src/main/java/service/AddressBookDBService.java
+++ src/main/java/service/AddressBookDBService.java
@@ -13,7 +13,7 @@ public class AddressBookDBService {
     public Connection getConnection() throws SQLException{
         String jdbcURL ="jdbc:mysql://localhost:3306/address_book_service?useSSL=true";
         String userName = "root";
-        String password = "priya@1987";
+        String password = "1234";
         Connection connection;
         System.out.println("Connecting to database");
         connection = DriverManager.getConnection(jdbcURL,userName,password);
@@ -53,4 +53,51 @@ public class AddressBookDBService {
         }catch(SQLException e){e.printStackTrace();}
         return contactList;
     }
+
+    public static void main(String[] args) {
+        Contact contact_old_det = new Contact("sushila","kumari","Ambedkar Nagar","chhindwara"
+                ,"Madhya Pradesh",456238,0.0,null);
+
+        Contact contact_new_det = new Contact("sushila","kumari","Ambedkar Nagar","chhindwara"
+                ,"Madhya Pradesh",456238,0.0,"sushila.kumari@gmail.com");
+        String sql1 = "UPDATE person_address SET email_id= "+ String.format(contact_new_det.getEmail())
+                        +"WHERE first_name="+String.format(contact_old_det.getFirst_name());
+        String sql = new StringBuilder().append("UPDATE person_address SET email_id=\" ").append(contact_new_det.getEmail())
+                .append("\" WHERE first_name=\"").append(contact_old_det.getFirst_name()).append("\"").toString();
+        System.out.println("sql with string builder = "+ sql1);
+        System.out.println("sql without builder" +sql);
+    }
+    public int updateData(Contact contact_old_det, Contact contact_new_det) {
+        // update email_id of the given contact
+        String sql = new StringBuilder().append("UPDATE person_address SET email_id= \"").append(contact_new_det.getEmail())
+                .append("\" WHERE first_name=\"").append(contact_old_det.getFirst_name()).append("\"").toString();
+        int rows = 0;
+        try(Connection connection = this.getConnection()){
+            Statement statement = connection.createStatement();
+            statement.executeUpdate(sql);
+            //System.out.println("resulteset+ "+ resultSet);
+            //rows = Integer.parseInt(String.valueOf(resultSet));
+        }catch(SQLException e){e.printStackTrace();}
+        return rows;
+    }
+
+    // Overloaded method, used to read rows of table where first name and last name match
+    public List<Contact> readData(String first_name, String last_name) {
+
+        // Read contact details based on the first name and last name
+        String sql = "SELECT * FROM person_address WHERE first_name=\""+ first_name +"\" AND last_name=\"" + last_name +"\"";
+        List<Contact> contactList = new ArrayList<>();
+
+        try(Connection connection = this.getConnection()){
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(sql);
+            contactList = getContactData(resultSet);
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+        return contactList;
+    }
 }
+
+
+
